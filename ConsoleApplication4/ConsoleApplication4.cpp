730c093fb46bfb9b55d47035c8d9b51ee669cec2
Bohdan <Bohdan@DESKTOP-PIR5C7D>
2023-10-13 20:39:55 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    int verst;
+
+    std::cout << "Enter the number of versts:\n";
+    std::cin >> verst;
+
+    double km;
+    km = verst * 1.067;
+
+    std::cout << "Your versts in kilometers are equal " << std::setprecision(4) << km;
+}
